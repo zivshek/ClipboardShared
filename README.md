851f4b357b6bea21d6bb47f7bff022d7b65f4d86
ziv <ziv.hao.shek@gmail.com>
2020-11-26 15:36:12 -0500

    Import chokidar, iconv-lite, and electron-store; Implement monitoring file changes, still have a decoding issue.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ClipboardSharedElectron
+# ClipboardShared
+A cross-platfom clipboard sharing (between the host and virtual machines) application using a local shared folder.
+
+I am using VMWare Workstation 15 Player, unfortunitely it doesn't support clipboard sharing between host and guest. It's a known issue and there is really no way around it. VirtualBox has good support for it, but I had trouble setting it to work with an Xbox controller, which is a requirement for my job.
+
+Need to specify a directory as a cmdline argument, eg. "D:\clipboardShared"
--- main.js
+++ main.js
@@ -1,23 +1,91 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const fs = require('fs');
+const chokidar = require('chokidar');
+const Store = require('electron-store');
+const iconv = require("iconv-lite");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, clipboard, ipcRenderer } = electron;
 
-let mainWindow;
+const store = new Store();
+const c_TextFile = "text.txt";
+const c_ImageFile = "img.png";
+const c_Encoding = "utf8";
+let sharedDir = "F:\\Downloads\\clipboard\\";
+let watcher;
 
-app.on('ready', function () {
-    mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWindow.html'),
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 500,
+        height: 125,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        resizable: false
+    });
+
+    win.removeMenu();
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
         slashes: true
     }));
+}
+
+app.on('ready', function () {
+    createWindow();
+    const files = [`${sharedDir}${c_TextFile}`, `${sharedDir}${c_ImageFile}`];
+    createWatcher(files);
+    let raw = fs.readFileSync(files[0], 'utf8');
+    let content = iconv.decode(raw, c_Encoding);
+    console.log(content);
+    startWatching();
 });
 
-// menu template
-const mainMenuTemplate = [
-    {
-        label: 'Settings'
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
     }
-];
+
+    watcher.close();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+function createWatcher(files) {
+    console.log(`Watching ${files}`);
+    watcher = chokidar.watch(files, {
+        ignored: /(^|[\/\\])\../, // ignore dotfiles
+        persistent: true
+    });
+}
+
+function startWatching() {
+    watcher.on('change', (file) => {
+        const filename = path.basename(file);
+        switch (filename) {
+            case c_TextFile:
+                try {
+                    let raw = fs.readFileSync(file);
+                    let content = iconv.decode(raw, c_Encoding);
+                    console.log(content);
+                    clipboard.writeText(content);
+                } catch (error) {
+                    console.log(error);
+                }
+                break;
+            case c_ImageFile:
+                break;
+        }
+    });
+}
+
+function stopWatching() {
+    watcher.on('change', () => { });
+}
